b352183fabccd8e471050c4f63a0496e00c46507
w-draper <willddude@gmail.com>
2019-04-03 10:01:15 +1100

    low level 1.0

--- cipher.c
+++ cipher.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 
-//first comment
+
+
+/***********************************************************
+This function takes an array and two integers to decrypt using 
+the rotation cipher. 
+The array holds the message to be decrypted.
+	The array at this stage will already be transformed from chars to ASCII
+One Int holds the 'key' for the encryption.
+One Int holds the length of the array.
+************************************************************/
+int rotEnc(int text, int key);
+
+
+
+int rotDec(/*inputs*/);
+ 
+
+
+int subEnc(/*inputs*/);
+
+
+
+int subDec(/*inputs*/);
+
+
+
+int a2c(/*inputs*/);
+
+
+
+
 int main(){
-	printf("hello world\n");
-	return 0;
+	printf("Initialising\n");
+	//take stdin of the text
+	//sent to function to encode in ascii
+	//send ascii to appropriate cipher funct
+	//int ascii = 65; //spells 'A' in ASCII
+	//int key = 1;
+	printf("%d\n", rotEnc(65, 1));
+	
+}
+
+int rotEnc(int text, int key){
+	int enc;
+	enc = (text + key); //* (% 26);
+	return enc;
 }
