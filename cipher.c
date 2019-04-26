790e6b815690c00978e995aace7f24653e04fc21
w-draper <47999605+w-draper@users.noreply.github.com>
2019-04-26 19:13:08 +1000

    most code commented

--- cipher.c
+++ cipher.c
@@ -3,25 +3,41 @@
 
 
 /***********************************************************
-
+This function encrypts a message with the rotation, or Caeser 
+cipher.
+Takes a string (message to encrypt), and also an int (key).
+This function will print the encrypted message to stdout 
+automatically
 ************************************************************/
 void rotEnc(char *message, int key);
 
 
 /***********************************************************
-
+This function decrypts a message encrypted with the rotation, 
+or Caeser cipher.
+Takes a string (message to decrypt), and also an int (key).
+This function will print the encrypted message to stdout 
+automatically
 ************************************************************/
 void rotDec(char *message, int key);
  
 
 /***********************************************************
-
+This function encrypts a message with the substitution 
+cipher.
+Takes a string (message to encrypt), and a second string (key).
+This function will print the encrypted message to stdout 
+automatically
 ************************************************************/
 void subEnc(char *message, char *key);
 
 
 /***********************************************************
-
+This function decrypts a message encrypted with the substitution 
+cipher.
+Takes a string (message to decrypt), and a second string (key).
+This function will print the encrypted message to stdout 
+automatically
 ************************************************************/
 void subDec(char *message, char *key);
 
@@ -44,64 +60,69 @@ int main(){
 	printf("(6) Substitution Cipher decryption without key\n");   
 	printf("please select your algorithm by typing its associated number and pressing enter\n");
 	
+	//Will loop until until selection is changed from 0 i.e. when the user selects one of the above options
 	while(selection == 0){
-	scanf("%d", &selection);
+	scanf("%d", &selection); //here selection is taken from stdin which means that the loop will stop next time
 	 
-	if((selection > 0) && (selection <= 6)){
-		switch(selection){
-			case 1:
+	//based on the number input for selecton user is sent to a case with corresponding number
+	switch(selection){ //each case takes a text string and key of either int (case 1&2) or string (case 4&5) from stdin
+			//this case will encrypt based off
+			case 1:				
 				printf("You selected: (1) Rotation cipher encryption\n"); 
 				
 				printf("please enter the Key:\n");
 				scanf("%d", &keyI);
 				
 				printf("Please enter your text followed by a '~' then press enter:\n");
-				scanf("%[^~]s",text);
+				scanf("%[^~]s",text); //the '[^~]' means that scanf will read stdin until it encounters a ~ character
+									  //this means spaces can be read
+				printf("Your encrypted text is:\n");				
+				rotEnc(text, keyI); //sends message and key to function to be encrypted
 				
-				printf("Your encrypted text is:\n");
-				rotEnc(text, keyI);
 				break;
 			case 2:
-				printf("You selected: (2) Rotation Cipher decryption with key\n"); 
-				
-				printf("please enter the Key:\n");
-				scanf("%d", &keyI);
-				
+				printf("You selected: (2) Rotation Cipher decryption with key\n"); 				
+				printf("please enter the Key:\n")
+				scanf("%d", &keyI)
+					
 				printf("Please enter your text followed by a '~' then press enter:\n");
 				scanf("%[^~]s",text);
-				
+									
 				printf("Your decrypted text is:\n");
-				rotDec(text, keyI);
+				rotDec(text, keyI); //sends message and key to function to decrypt
+					
 				break;
 			case 3:
 				printf("You selected: (3) Rotation Cipher decryption without key\n"); 
-				
+					
 				printf("Sorry this feature is still under construction,\n please select another algorithm\n");
-				selection = 0;
+				selection = 0; //resets selection so that the user can re enter a selection with the while loop
 				break;
 			case 4:
 				printf("You selected: (4) Substitution Cipher encryption\n"); 
-				
+					
 				printf("please enter the Key followed by a '~' then press enter:\n");
-				scanf("%s", key);
-				
+				scanf("%s", key);//note the key variable is changed to accomodate for a string key not int
+					
 				printf("Please enter your text followed by a '~' then press enter:\n");
 				scanf("%[^~]s",text);
-				
+					
 				printf("Your encrypted text is:\n");
 				subEnc(text, key);
+					
 				break;
 			case 5:
 				printf("You selected: (5) Substitution Cipher decryption with key\n"); 
-				
+					
 				printf("please enter the Key followed by a '~' then press enter:\n");
-				scanf("%s", key);
-				
+				scanf("%s", key); //note the key variable is changed to accomodate for a string key not int
+					
 				printf("Please enter your text followed by a '~' then press enter:\n");
 				scanf("%[^~]s",text);
-				
+					
 				printf("Your decrypted text is:\n");
-				subDec(text, key);
+				subDec(text, key); //sends message and key to subDec to encrypt
+			
 				break;
 			case 6:
 				printf("You selected: (6) Substitution Cipher decryption without key\n"); 
@@ -109,15 +130,13 @@ int main(){
 				printf("Sorry this feature is still under construction,\n please select another algorithm\n");
 				selection = 0;
 				break;
-
+			//if the user selects a number not on the list then they are sent here (user proofing)
+			default:
+				printf("please enter a number that matches one of the above sections:");
+				selection = 0;
+				break;
 		}
 	}
-	else
-		printf("please enter a number that matches one of the above sections:");
-	}
-
-
-	
 }
 
 void rotEnc(char *message, int key){
@@ -127,19 +146,18 @@ void rotEnc(char *message, int key){
 	
 	//this for loop will loop for every value of the array 'text[]'
 	for(i = 0; message[i] != 0; ++i){         //loops for every letter
-		val = message[i]; 
-		//printf("%d\n", val);              //the letter is assigned to val
+		val = message[i]; 					//the letter is assigned to val
+							             
 		if((val >= 'a') && (val <= 'z')){  //if the letter is between a and z
 			val = val + key;               //add the key to the letter ascii val
 			
 			if(val > 'z'){                 //if ascii val is larger than z when added
 				val = val - 'z' + 'a' - 1; //swap it around to the start of alphabet
 			}
-			message[i] = val;                 //set the encryted letter to its place in
-			//printf("%c", val);
-			}                              //array                              	
+			message[i] = val;                 //set the encryted letter to its place in the array
+			}                                                            	
 	}
-	printf("%s\n", message);	
+	printf("%s\n", message); //after all looping print the final message	
 }
 
 
@@ -149,19 +167,18 @@ void rotDec(char *message, int key){
 	
 	//this for loop will loop for every value of the array 'text[]'
 	for(i = 0; message[i] != 0; ++i){         //loops for every letter
-		val = message[i]; 
-		//printf("%d\n", val);              //the letter is assigned to val
+		val = message[i]; 					 //the letter is assigned to val
+						             
 		if((val >= 'a') && (val <= 'z')){  //if the letter is between a and z
 			val = val - key;               //subtract the key to the letter ascii val
 			
 			if(val < 'a'){                 //if ascii val is less than a when decrypted
 				val = val + 'z' - 'a' + 1; //swap it around to the end of alphabet
 			}
-			message[i] = val;                 //set the encryted letter to its place in
-			//printf("%c", val);
-			}                              //array                              	
+			message[i] = val;                 //set the encryted letter to its place in the array 
+			}                                                           	
 	}
-	printf("%s\n", message);	
+	printf("%s\n", message); //after all looping print the final message		
 }
 
 void subEnc(char *message, char *key){
@@ -179,12 +196,9 @@ void subEnc(char *message, char *key){
 		for(i1 = 0; alphabet[i1] != 0; ++i1){
 			alph = alphabet[i1];
 			if(val == alph)
-				enc = i1;
-				
+				enc = i1;		
 		}
-		
-		
-		
+
 		keyA = key[enc];
 		
 		
@@ -209,12 +223,9 @@ void subDec(char *message, char *key){
 		for(i1 = 0; key[i1] != 0; ++i1){
 			keyA = key[i1];
 			if(val == keyA)
-				enc = i1;
-				
+				enc = i1;		
 		}
-		
-		
-		
+
 		alph = alphabet[enc];
 		
 		
